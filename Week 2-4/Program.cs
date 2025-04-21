3baf2bdc1b40a6e82a25e2b8f8c33f40a81e018e
xbatm <dbustos28@amarillocollege.com>
2025-04-20 22:49:16 -0500

    Add project files.

---
+++ Week 2-4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c; // int
+            Console.WriteLine("Welcome to Hypotenuse Calculator"); // the calculator
+            Console.WriteLine("Enter the length of the first side (a): "); // first length
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the length of the second side (b): "); // second length
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Math.Sqrt(c = a * a + b * b)); // the total for c
+
+            Console.WriteLine($"the length of the hypotenuse is {c}"); // result
+            Console.WriteLine("thank you for using the Hypotenuse Calculator"); // thank you
+
+            Console.ReadLine(); //read line
+        }
+    }
+}
